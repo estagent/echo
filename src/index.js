4d7176aac207e2c0bfb856b52c09b14076af1a17
estagent <estagent@gmail.com>
2022-03-22 22:14:28 +0300

    5.6.2

--- src/index.js
+++ src/index.js
@@ -1,58 +1,61 @@
-import Client from './client'
+import Events from '@revgaming/events'
+import Echo from 'laravel-echo'
+import Pusher from 'pusher-js'
 
 let client
 let privateChannel
+let usersRoom
 
-const channels = []
+window.Pusher = Pusher
 
-const connect = options => {
-  client = new Client(options)
-  privateChannel = client.private(`App.user.${options.user}`)
-  return client
-}
+export default (instance, options = {}) => {
+  window.addEventListener(Events.UserMounted, event => {
+    window.Echo = client = new Echo({
+      broadcaster: 'pusher',
+      key: options.key,
+      wsHost: options.host,
+      wsPort: options.port,
+      wssPort: options.port,
+      forceTLS: false,
+      encrypted: true,
+      disableStats: true,
+      enabledTransports: ['ws', 'wss'],
+      authorizer: (channel, opts) => {
+        return {
+          authorize: (socketId, callback) => {
+            instance
+              .post(opts.authEndpoint, {
+                socket_id: socketId,
+                channel_name: channel.name,
+              })
+              .then(response => {
+                callback(false, response.data)  // response.data from axios.
+              })
+              .catch(error => {
+                callback(true, error)
+              })
+          },
+        }
+      },
+    })
+    privateChannel = client.private(`App.user.${event.detail.user.id}`)
+    usersRoom = client.join('users')
+  })
 
-const disconnect = () => {
-  if (client) {
-    if (privateChannel) {
-      privateChannel.unsubscribe()
-      privateChannel = null
-    }
-
-    for (let chanel of channels) {
-      client.leave(chanel)
-    }
-
+  window.addEventListener(Events.UserUnmounted, () => {
+    usersRoom.unsubscribe()
+    usersRoom = null
+    privateChannel.unsubscribe()
+    privateChannel = null
     client.disconnect()
     client = null
+  })
+
+  return {
+    listenForAuthenticated: callback => {
+      privateChannel.notification(callback)
+      privateChannel.listen('.NewMessage', callback)
+      usersRoom.listen('.NewMessage', callback)
+    },
   }
-}
-
-const join = name => {
-  if (channels.includes(name)) {
-    throw 'already joined'
-  }
-
-  const chanel = client.join(name)
-  channels.push(name)
-  return chanel
-}
-
-const leave = name => {
-  if (!channels.includes(name)) throw 'not joined joined'
-
-  client.leave(name)
-  for (let i = 0; i < channels.length; i++) {
-    if (channels[i] === name) {
-      channels.splice(i, 1)
-    }
-  }
-  return channels.length
-}
-
-export default {
-  connect: connect,
-  disconnect: disconnect,
-  privateChannel: () => privateChannel,
-  join: join,
-  leave: leave,
 }
